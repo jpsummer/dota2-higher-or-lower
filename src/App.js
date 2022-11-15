93a696fcfca5d507d90fd5d8158ce3f1bcccc3dd
Jacob Somerville <j.somerville97@gmail.com>
2022-11-15 13:06:11 +1030

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex flex-row justify-evenly items-center bg-red-200 min-h-screen">
+            <div className="rounded-xl bg-blue-700">
+                <h1 className="text-3xl text-green-700 font-bold underline">
+                    Hello world!
+                </h1>
+            </div>
+            <div className="rounded-xl bg-blue-700">
+                <h1 className="text-3xl text-green-700 font-bold underline">
+                    Hello world!
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
