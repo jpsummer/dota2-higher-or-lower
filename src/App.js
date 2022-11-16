0f8d5f5e9aef680692302f768ee1abd25d0b7ec4
Jacob Somerville <j.somerville97@gmail.com>
2022-11-16 15:58:41 +1030

    rough skeleton of ui

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import Card from './Card.js';
 
 function App() {
     return (
-        <div className="flex flex-row justify-evenly items-center bg-epic min-h-screen">
-            <Card />
-            <Card />
+
+        <div className="flex flex-col justify-around items-center min-h-screen">
+            <div className="">
+                <h1 className="lg:text-9xl md:text-8xl text-6xl text-white">Higher or Lower</h1>
+            </div>
+            <div className="flex flex-col justify-evenly grow w-8/12">
+                <div className="flex flex-row justify-between">
+                    <Card />
+                    <Card />
+                </div>
+                <div className="flex flex-row justify-end">
+                    <button className="bg-blue-700 hover:bg-blue-400 text-2xl text-white font-bold py-4 px-8 mx-2 border-b-4 border-blue-700 hover:border-blue-500 rounded-xl">Higher</button>
+                    <button className="bg-blue-700 hover:bg-blue-400 text-2xl text-white font-bold py-4 px-8 mx-2 border-b-4 border-blue-700 hover:border-blue-500 rounded-xl">Lower</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 export default function Card() {
     return (
-        <div className="rounded-xl bg-blue-700 p-10">
+        <div className="rounded-xl bg-blue-700 p-5 m-5">
             <h1 className="text-3xl text-slate-50 font-bold">
                 Hello world!
             </h1>
